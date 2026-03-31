be9af4ac8160723a0bcbc22a0db9046a99993575
Adrián Pérez Domínguez <adriperezdomin@gmail.com>
2026-03-31 16:07:43 +0200

    Conexión con la InfluxDB lista, .env en el .gitignore porque ahora contiene el token de la influxdb entre otras cosas

--- ingest/subscriber.py
+++ ingest/subscriber.py
@@ -1,6 +1,22 @@
 import paho.mqtt.client as mqtt
 import json
 from utils.sliding_window import window_update, window_alarm
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+INFLUX_URL = os.getenv("INFLUX_URL")
+INFLUX_TOKEN = os.getenv("INFLUX_TOKEN")
+INFLUX_ORG = os.getenv("INFLUX_ORG")
+INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
+
+
+influx_client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
+write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
 # ── 1. Callback: conexión ──────────────────────────────────────────
 def on_connect(client, userdata, flags, rc):
@@ -18,27 +34,62 @@ def on_message(client, userdata, msg):
     payload = msg.payload.decode()
 
 
-    print("\nTopic:", msg.topic)
+    topic_split = msg.topic.split('/')
+    if len(topic_split) < 4:
+        return
+
+    sector = topic_split[1]
+    sensor_id = topic_split[2]
+    msg_type = topic_split[3]
+
+    print(f"\n[TOPIC] {msg.topic}")
+
+    # Prepara el objeto point, necesario para InfluxDB
+    point = Point("rocket_metrics") \
+        .tag("system", sector) \
+        .tag("sensor", sensor_id)
+
     
-    if msg.topic.endswith("/data"):
-        try:
-            #1. Crear ventana deslizante y ajustarla a la temperatura 
-            data = json.loads(payload)
-            print("Data:", data)
-            
-        except json.JSONDecodeError:
-            print("JSON Error:", payload)
 
-        if msg.topic.endswith("/s-termometer-01/data"):
-            window_update(data['value'], sliding_window_termometer, 5)
-            window_alarm(sliding_window_termometer, alarm_threshold=810.0, data='temperatura')
-            
-        if msg.topic.endswith("/s-barometer-01/data"):
-            window_update(data['value'], sliding_window_barometer, 10)
-            window_alarm(sliding_window_barometer, alarm_threshold=800.0, data='presion')
-            
+    if msg_type == "data":
+        data = json.loads(payload)
 
-    else:
+        if 'value' in data:
+            valor = float(data['value'])
+            point.field("value", valor)
+            
+            # Tu lógica de alarmas (solo si hay 'value')
+            if sensor_id == "s-termometer-01":
+                window_update(valor, sliding_window_termometer, 5)
+                window_alarm(sliding_window_termometer, 810.0, 'temperatura')
+            elif sensor_id == "s-barometer-01":
+                window_update(valor, sliding_window_barometer, 10)
+                window_alarm(sliding_window_barometer, 800.0, 'presion')
+
+        elif "acceleration_data" in data and "gyroscope_data" in data:
+                acc = data["acceleration_data"]
+                gyro = data["gyroscope_data"]
+                
+                # Aplanamos los datos para InfluxDB
+                point.field("acc_x", float(acc[0]))
+                point.field("acc_y", float(acc[1]))
+                point.field("acc_z", float(acc[2]))
+                point.field("gyro_x", float(gyro[0]))
+                point.field("gyro_y", float(gyro[1]))
+                point.field("gyro_z", float(gyro[2]))
+                
+                # Tags adicionales que vienen en tu JSON
+                point.tag("measure_type", data.get("type", "direction"))
+                point.tag("unit", data.get("unit", "m/s2_rad/s"))
+
+        # Escribir en InfluxDB
+        write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
+        print("Envio exitoso a InfluxDB!")
+    
+
+    elif msg_type == "status":
+        point.field("status_msg", payload)
+        write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
         print("Status message:", payload)
     
         
